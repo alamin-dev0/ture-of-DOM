a198303b5f6da56906fb8e05840784ba4e2037f2
Al Amin <alamin.dev01@gmail.com>
2026-02-18 02:46:59 +0600

    added some code

---
+++ js/cssInJs.js
@@ -0,0 +1,18 @@
+// const section = document.getElementsByTagName('section')
+const sections = document.querySelectorAll('section')
+// console.log(section)
+// for(const section of sections){
+//     console.log(section)
+//     section.style.backgroundColor = 'gray'
+//     section.style.border = 'solid 2px green'
+//     section.style.marginBottom = '20px'
+//     section.style.borderRadius = '15px'
+//     section.style.padding = '30px'
+// }
+
+
+
+// dynamic class
+for(const section of sections ){
+    section.classList.add('section-cart')
+}
